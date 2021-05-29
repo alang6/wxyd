99cb86086bb6884693454984f9fa0fec4e165f84
gitupdate <80134249+gitupdate@users.noreply.github.com>
2021-05-30 01:08:48 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -19,7 +19,7 @@ let SCKEY = '';
 
 // =======================================Bark App通知设置区域===========================================
 //此处填你BarkAPP的信息(IP/设备码，例如：https://api.day.app/XXXXXXXX)
-let BARK_PUSH = 'RFzUy9BTszQzqZvyh6Ygi4';
+let BARK_PUSH = '';
 //BARK app推送铃声,铃声列表去APP查看复制填写
 let BARK_SOUND = '';
 
