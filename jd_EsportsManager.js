47df08526b2ce44aab0d2e8e01675af0304a2a63
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-06-29 15:48:49 +0800

    Update jd_EsportsManager.js

--- jd_EsportsManager.js
+++ jd_EsportsManager.js
@@ -29,7 +29,6 @@
 const $ = new Env('东东电竞经理');
 let cookiesArr = [], cookie = '', isBox = false, notify, newShareCodes, allMessage = '';
 let tasks = [], shareCodes = [], first = true;
-beizhuli = ""
 
 !(async () => {
   await requireConfig();
@@ -92,7 +91,7 @@ async function main() {
         await getAssist()
         await $.wait(1000)
 
-        console.log(`第${$.index}个账号${$.UserName}去助力第${beizhuli}个账号。`)
+        console.log(`第${$.index}个账号${$.UserName}去助力第${Math.floor(($.index - 1) / 6) + 1}个账号。`)
         await doAssist()
         continue
       case 2:
@@ -158,7 +157,6 @@ function getShareCode(token) {
 }
 
 function doAssist() {
-  beizhuli=Math.floor(Math.random() * 15 )
   return new Promise(resolve => {
     $.post({
       url: 'https://xinruidddj-isv.isvjcloud.com/api/task/do_assist_task',
@@ -170,8 +168,7 @@ function doAssist() {
         'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;10.0.2;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
         'Accept-Language': 'zh-cn',
       },
-      //body: `token=${shareCodes[Math.floor(($.index - 1) / 6)].tid}&inviter=${Math.floor(($.index - 1) / 6).uid}`
-      body: `token=${shareCodes[beizhuli].tid}&inviter=${beizhuli.uid}`
+      body: `token=${shareCodes[Math.floor(($.index - 1) / 6)].tid}&inviter=${Math.floor(($.index - 1) / 6).uid}`
     }, (err, resp, data) => {
       try {
         data = $.toObj(data)
