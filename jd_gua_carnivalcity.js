52fe5084bbb9a6544affc63416abe4db31c55459
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-08-09 14:52:01 +0800

    Update jd_gua_carnivalcity.js

--- jd_gua_carnivalcity.js
+++ jd_gua_carnivalcity.js
@@ -61,7 +61,7 @@ let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*
     if ($.isNode()) await notify.sendNotify($.name + '活动已结束', `请删除此脚本\n咱江湖再见`);
     return
   }
-  await updateShareCodesCDN();
+  //await updateShareCodesCDN();
   // await requireConfig();
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
