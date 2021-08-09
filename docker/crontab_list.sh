540afeeaa350db8f1dd5be42928b339229a99e05
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-08-09 16:57:32 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,6 +5,9 @@
 
 ##############短期活动##############
 
+# 京东手机狂欢城 助力
+0 0,6 9-28 8 * node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1
+
 # 京东手机狂欢城
 0 0-18/6 9-28 8 * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 
