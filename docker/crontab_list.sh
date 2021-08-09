0b8285548b89ac72262b5d58e8ccd79baa857c3c
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-08-09 10:53:06 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -11,9 +11,6 @@
 # 京喜领88元红包(8.31结束)
 0 0,12,21 * 7-8 * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 
-# 沃尔玛畅玩88(7.15-8.8)
-0 0,12,18 * 7-8 * node /scripts/jd_walmart.js >> /scripts/logs/jd_walmart.log 2>&1
-
 # 跳跳乐瓜分京豆(7.20-7.27)
 15 */2 20-27 7 * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 
