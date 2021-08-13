8c28bb279ac6dfd231e202ca1d403861bcb0f32b
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-08-13 11:48:44 +0800

    Update 过期活动的定时.sh

--- docker/过期活动的定时.sh
+++ docker/过期活动的定时.sh
@@ -9,9 +9,6 @@
 # 燃动夏季百元守卫战_助力 活动时间：7.8-8.8
 14,41 7-14 * 7-8 * node /scripts/jd_summer_movement_help.js >> /scripts/logs/jd_summer_movement_help.log 2>&1
 
-# 送豆得豆
-16 2,14 * * * node /scripts/jd_sendBeans.js >> /scripts/logs/jd_sendBeans.log 2>&1
-
 # 京享值PK 活动时间：6.22-7.21
 11 0,6,11,16,21 5-21 7 * node /scripts/jd_jxzpk.js >> /scripts/logs/jd_jxzpk.log 2>&1
 
