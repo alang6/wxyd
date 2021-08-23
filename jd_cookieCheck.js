dafa854f69ff536ab99c9f08213cc726853904cb
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-08-23 22:14:45 +0800

    Update jd_cookieCheck.js

--- jd_cookieCheck.js
+++ jd_cookieCheck.js
@@ -40,8 +40,9 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
             $.isLogin = true;
             $.nickName = '';
             message = '';
-            await GetJDUserInfoUnion();
             console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
+            await GetJDUserInfoUnion();
+
             if (!$.isLogin) {
                 $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
 
