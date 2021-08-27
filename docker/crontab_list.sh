68bef546a3d3b73cd8b013ce431616f2a479d6f0
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-08-27 09:35:34 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -14,12 +14,6 @@
 # 骁龙品牌日
 11 6,20 13-25 8 * node /scripts/jd_xl.js >> /scripts/logs/jd_xl.log 2>&1
 
-# 京东手机狂欢城 助力
-0 0,6 9-28 8 * node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1
-
-# 京东手机狂欢城
-0 0-18/6 9-28 8 * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
-
 # 来电好物季( 玩一玩- 来电好物季 )
 11 0 9-25 8 * node /scripts/jd_ldhwj.js >> /scripts/logs/jd_ldhwj.log 2>&1
 
