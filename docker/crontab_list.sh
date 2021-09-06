1f4c95ff34d5bf930569baaed4363b1bf3cee9d4
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-09-06 13:57:33 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -184,4 +184,3 @@
 6 * * * * node /scripts/jd_cfd.js >> /scripts/logs/jd_cfd.log 2>&1
 # 超级直播间红包雨(活动时间不定期，出现异常提示请忽略。红包雨期间会正常)
 #0,30 0-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
-
