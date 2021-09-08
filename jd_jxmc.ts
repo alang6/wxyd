ddf270ede1069fc5fca0b6a667bce6f0e863fcac
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-09-08 19:37:50 +0800

    Update jd_jxmc.ts

--- jd_jxmc.ts
+++ jd_jxmc.ts
@@ -12,7 +12,7 @@ import {accessSync} from "fs";
 
 const notify = require('./sendNotify')
 
-let A: any = require('./utils/jd_jxmcToken')
+let A: any = require('./utils/jd-jxmcToken')
 let cookie: string = '', res: any = '', shareCodes: string[] = [], homePageInfo: any, activeid: string = '', jxToken: any, UserName: string, index: number;
 let HELP_HW: string = process.env.HELP_HW ? process.env.HELP_HW : "true";
 console.log('帮助HelloWorld:', HELP_HW)
