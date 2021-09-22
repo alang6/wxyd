2bd4284570ae9a4aa895367b9d014822db162ee4
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-09-22 10:52:09 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -447,7 +447,7 @@ function getMs() {
         } else {
           if (safeGet(data)) {
             data = JSON.parse(data)
-            if (data.code === 2042) {
+            if (data.code === 2041) {
               $.JdMsScore = data.result.assignment.assignmentPoints || 0              
             }
           }
